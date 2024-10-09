6d03b81466255e075ec270d5639789cfddcef3e9
Naman Sharma <namansharma3007c@gmail.com>
2024-10-09 06:40:11 +0530

    bug: fixed bug in side bar

--- components/SideSlideBar.tsx
+++ components/SideSlideBar.tsx
@@ -32,7 +32,7 @@ export default function SideSlideBar({
   return (
     <div
       className={`${
-        openSideBar ? "right-0" : "-right-[50rem]"
+        openSideBar ? "right-0" : "-right-[60rem]"
       } w-1/2 h-full bg-transparent fixed right-0 top-0 z-20 py-4 px-4 ease-in-out duration-300`}
     >
       <div className="w-full h-full bg-white rounded-lg flex flex-col py-4 px-6 shadow-md overflow-hidden">
